5acb74e941197d374c950f00b5e0daea434fc7fc
dymafr <erwan1309@hotmail.com>
2022-06-28 14:44:41 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useRef, forwardRef } from 'react';
-
-const MyInput = forwardRef((props, ref) => {
-  {
-    return <input ref={ref} className="m-10" type="text" />;
-  }
-});
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const inputRef = useRef(null);
+  console.log('Rendu du composant App');
+  const [count, setCount] = useState(0);
+  const [title, setTitle] = useState('Dyma');
 
-  function handleClick() {
-    inputRef.current.focus();
-  }
+  useEffect(() => {
+    console.log('Effet !');
+    document.title = title;
+  }, [title]);
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-20">
-      <input ref={inputRef} className="m-10" type="text" />
-      <button className="btn btn-primary" onClick={handleClick}>
-        Focus champ
+      <input
+        className="m-10"
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+      />
+      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+        {count}
       </button>
     </div>
   );
